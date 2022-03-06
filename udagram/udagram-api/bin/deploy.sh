9dd64126276c34b10bfa52535e2db8f8d8ab76f9
DeathL1sT <you@example.comaymansamir45@gmail.com>
2022-03-06 20:09:40 +0200

    init eb arn

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -1,5 +1,4 @@
-eb init udagram
+eb init eb init arn:aws:s3:::elasticbeanstalk-us-east-1-312921138927/.elasticbeanstalk
 cd www
 eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME POSTGRES_PASSWORD=$POSTGRES_PASSWORD POSTGRES_DB=$POSTGRES_DB PORT=$PORT POSTGRES_HOST=$POSTGRES_HOST AWS_REGION=$AWS_REGION AWS_PROFILE=$AWS_PROFILE AWS_BUCKET=$AWS_BUCKET URL=$URL JWT_SECRET=$JWT_SECRET
-eb use Udagram-env
-eb deploy
+eb deploy Udagram-env
