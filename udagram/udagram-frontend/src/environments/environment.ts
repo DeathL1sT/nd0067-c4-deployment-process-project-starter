d59f2cef1ed2072feab80743219ee0ce858fda35
DeathL1sT <you@example.comaymansamir45@gmail.com>
2022-03-07 16:51:17 +0200

    cors problem https

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://Udagram-env.eba-zmbtdm7x.us-east-1.elasticbeanstalk.com'
+  apiHost: 'https://Udagram-env.eba-zmbtdm7x.us-east-1.elasticbeanstalk.com'
 };
 
 /*
