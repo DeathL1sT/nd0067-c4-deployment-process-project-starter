4dd446ff89d6cce448151acf5ab36a58deda870c
DeathL1sT <you@example.comaymansamir45@gmail.com>
2022-03-07 14:12:04 +0200

    fix eb link

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "https://elasticbeanstalk-us-east-1-312921138927.s3.amazonaws.com/.elasticbeanstalk/api/v0/",
+  apiHost: "https://Udagram-env.eba-zmbtdm7x.us-east-1.elasticbeanstalk.com/api/v0/",
 };
 
 /*
